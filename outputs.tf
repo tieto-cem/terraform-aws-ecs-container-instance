153467832108b0be5cdb10b1dec1b33a4ec49778
Jarl <jarl.tornroos@tieto.com>
2018-11-23 18:49:02 +0200

    Exporting ASG name

--- outputs.tf
+++ outputs.tf
@@ -3,6 +3,11 @@ output "asg_id" {
   value       = "${aws_autoscaling_group.asg.id}"
 }
 
+output "asg_name" {
+  description = "ASG name"
+  value       = "${aws_autoscaling_group.asg.name}"
+}
+
 output "lc_id" {
   description = "Launch Configuration id"
   value      = "${aws_launch_configuration.lc.id}"
